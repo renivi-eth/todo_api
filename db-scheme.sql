a8728a9a1d4bc78e49ae51680072212b789581c3
Egor_mac <ivinegor88@gmail.com>
2024-06-30 02:53:11 +0300

    fix db-scheme.sql

--- db-scheme.sql
+++ db-scheme.sql
@@ -1,14 +1,32 @@
--- Тут нужно описать DDL для создания таблиц и индексов в базе данных
-/* Пример:
-    CREATE TABLE users (
-       id SERIAL PRIMARY KEY,
-       name VARCHAR(255) NOT NULL
-    );
+create table users
+(
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    email varchar (256),
+    password text not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
 
-    CREATE INDEX users_name_idx ON users (name);
+create table tasks (
+    id uuid primary key default gen_random_uuid(),
+    name varchar(30) not null,
+    description text,
+    state task_state not null default 'backlog',
+    user_id uuid references users(id),
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
 
-    CREATE TABLE ...
-*/
+create table tag (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name varchar(50),
+    user_id uuid references users(id),
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
 
--- TODO: Описать DDL таблиц базы данных
--- TODO: Комментарии выше потом удалить
+create table task_tag (
+    task_id uuid  references tasks(id),
+    tag_id uuid references tag(id),
+    primary key (task_id, tag_id)
+);
