7ddda41a0ebccefedcd0c06dcd27bb674d206c75
Demonov <rustamdemonov@gmail.com>
2024-06-29 22:52:46 +0500

    change app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,22 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
-import { router } from './task/task.controller';
 import dotenv from 'dotenv';
+import { router } from './task/task.controller';
 import { routerTags } from './tags/tags.controller';
 import { authRouter } from './auth/auth.controller';
 
+// Инициализация переменных окружения
 dotenv.config();
 
-const username = encodeURIComponent(process.env.USERNAME_MONGODB as string);
-const password = encodeURIComponent(process.env.PASSWORD_MONGODB as string);
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.APP_PORT ?? 3000;
 
+// Подключение к базе данных
+// TODO: Убрать в отдельный файл. Заменить на PostgreSQL
 mongoose
-  .connect(
-    `mongodb+srv://${username}:${password}@cluster0.uhacroy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
-    {
-      dbName: 'todoDB',
-    },
-  )
+  .connect(`mongodb+srv:/@cluster0.uhacroy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
+    dbName: 'todoDB',
+  })
   .then(() => {
     console.log('Succesfull to connect Database');
   })
@@ -27,16 +24,20 @@ mongoose
     console.log(error);
   });
 
+// Парсинг JSON в теле запроса. Должен быть выше роутеров
 app.use(express.json());
 
+// Роутеры
 app.use(router);
 app.use(routerTags);
 app.use(authRouter);
 
+// Обработка ошибок
 app.use((req, res) => {
   res.status(404).send('Not found');
 });
 
+// Запуск сервера
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
