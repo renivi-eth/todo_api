565e8e7e3bd11dc4b9faaba59277f32694e99068
Egor_mac <ivinegor88@gmail.com>
2024-07-06 22:47:30 +0300

    add db query

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -18,5 +18,17 @@ routerTags.get(
   // @ts-ignore
   authMiddleware,
   // @ts-ignore
-  async (req: Request & { user: IUserJWT }, res: Response) => {},
+  async (req: Request & { user: IUserJWT }, res: Response) => {
+    const id = req.params.id;
+    const userId = req.user.id;
+
+    const {
+      rows: [tags],
+    } = await db.query(
+      'SELECT * FROM task_tag JOIN tag ON task_tag.tag_id = tag.id WHERE task_id = $1 AND user_id = $2',
+      [id, userId],
+    );
+
+    return res.status(200).send(tags.name);
+  },
 );
