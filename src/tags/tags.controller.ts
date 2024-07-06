355e202b5d081acfd6b9d3f908b41cf73278386c
Egor_mac <ivinegor88@gmail.com>
2024-07-06 22:40:43 +0300

    add validate / auth middleware for get tags point

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -9,4 +9,14 @@ import db from '../lib/database';
 
 export const routerTags = express.Router();
 
-routerTags.get('/tags/:id');
+routerTags.get(
+  '/tags/:id',
+
+  param('id', 'ID must be UUID').trim().notEmpty().isUUID(),
+
+  validateQuery,
+  // @ts-ignore
+  authMiddleware,
+  // @ts-ignore
+  async (req: Request & { user: IUserJWT }, res: Response) => {},
+);
