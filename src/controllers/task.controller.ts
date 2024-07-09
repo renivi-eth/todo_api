d0ee3fa328f49d089cb7a331638655d8eaed45ed
Egor_mac <ivinegor88@gmail.com>
2024-07-09 19:29:36 +0300

    fix delete method

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -71,7 +71,7 @@ router.post(
   '/task',
 
   authMiddleware,
-  // TODO: вынести все отдельно; 
+  // TODO: вынести все отдельно;
   body('name', 'Name must be a string').isString().isString().trim().isLength({ min: 0, max: 30 }),
   body('description', 'Description must be a text').isString().trim(),
   body('state', 'State must be only backlog, in-progress or done').isIn(['backlog', 'in-progress', 'done']),
@@ -99,71 +99,57 @@ router.post(
 router.put(
   '/task/:id',
 
+  authMiddleware,
+
   param('id', 'ID must be UUID').trim().notEmpty().isUUID(),
-  body('name', 'Name is required or must be min 3 and max 300 symbols')
-    .isString()
-    .isLength({ min: 3, max: 300 })
-    .notEmpty(),
-  body('description', 'Field description must be a string and max 1000 symbols')
-    .optional()
-    .isLength({ min: 0, max: 1000 })
-    .isString(),
-  body('completed', 'Completed must be boolean value').optional().trim().notEmpty().isBoolean(),
-  body('tags', 'Tags must be array').optional().isArray({ min: 0, max: 30 }),
-  body('state', 'State must be only backlog, in-progress or done')
-    .optional()
-    .notEmpty()
-    .isString()
-    .isIn(['backlog', 'in-progress', 'done']),
+  body('name', 'Name must be a string').isString().isString().trim().isLength({ min: 0, max: 30 }),
+  body('description', 'Description must be a text').isString().trim(),
+  body('state', 'State must be only backlog, in-progress or done').isIn(['backlog', 'in-progress', 'done']),
 
   handleReqQueryError,
 
-  // @ts-ignore
-  authMiddleware,
-
-  // @ts-ignore
   async (req: AppRequest, res: Response) => {
-    const id = req.params.id;
+    if (!req.user) {
+      throw new Error('User not found');
+    }
+
     const { name, description, state } = req.body;
-    const userId = req.user?.id;
 
     const {
       rows: [task],
     } = await db.query(
       'UPDATE task SET name = $1, description = $2, state = $3, updated_at = NOW() WHERE id = $4 AND user_id = $5 RETURNING *',
-      [name, description, state, id, userId],
+      [name, description, state, req.params.id, req.user.id],
     );
 
     if (!task) {
-      res.status(400).send('Task not found or not authorized to delete this task!');
+      return res.status(400).send('Task not found or not authorized to delete this task!');
     }
 
-    res.status(201).send(task);
+    return res.status(201).send(task);
   },
 );
 
 router.delete(
   '/task/:id',
 
+  authMiddleware,
+
   param('id').trim().notEmpty().isUUID(),
 
   handleReqQueryError,
 
-  // @ts-ignore
-  authMiddleware,
+  async (req: AppRequest, res: Response) => {
+    if (!req.user) {
+      throw new Error('User not found');
+    }
 
-  // @ts-ignore
-  async (req: AppRequest & { user: IUserJWT }, res: Response) => {
-    const id = req.params.id;
-    const userId = req.user.id;
-
-    console.log(id, userId);
     const {
       rows: [task],
-    } = await db.query('DELETE FROM task WHERE id = $1 AND user_id = $2 RETURNING *', [id, userId]);
+    } = await db.query('DELETE FROM task WHERE id = $1 AND user_id = $2 RETURNING *', [req.params.id, req.user.id]);
 
     if (!task) {
-      res.status(404).send('Task not found or not authorized to delete this task');
+      return res.status(404).send('Task not found or not authorized to delete this task');
     }
     return res.status(200).send(task);
   },
