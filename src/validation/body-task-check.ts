098a0bf06c909d78679146aeff72e2f5028f3828
Egor_mac <ivinegor88@gmail.com>
2024-07-22 02:40:59 +0300

    fix(validation): add Object.values for enum in check state in body

--- src/validation/body-task-check.ts
+++ src/validation/body-task-check.ts
@@ -1,10 +1,13 @@
 import { body, ValidationChain } from 'express-validator';
+import { TaskState } from '../lib/variables/task-state';
 
-// TODO: Убрать все .trim()
+// TODO: убрать все trim()
 
+/*
+Валидация данных в теле запроса (body) при создании задачи (POST)
+*/
 export const bodyTaskCheck: ValidationChain[] = [
-  body('name', 'Name must be a string').isString().isString().trim().isLength({ min: 0, max: 30 }),
-  body('description', 'Description must be a text').isString().trim(),
-  // TODO: вынести ['backlog', 'in-progress', 'done'] в enum
-  body('state', 'State must be only backlog, in-progress or done').isIn(['backlog', 'in-progress', 'done']),
+  body('name', 'Name must be a "string", not over 30 symbols').isString().isLength({ min: 0, max: 30 }),
+  body('description', 'Description must be a text').isString(),
+  body('state', 'State must be only backlog, in-progress or done').isIn([Object.values(TaskState)]),
 ];
