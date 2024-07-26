7bb625acc21a2ba4166c4e39537fcc8fc926e17f
ivin <ivinegor88@gmail.com>
2024-07-27 02:17:02 +0300

    feat(controllers): add new logic on GET with query param

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,23 +1,18 @@
 import express, { Response } from 'express';
-import { param } from 'express-validator';
 
 import { knex } from '../database';
 
 import { AppRequest } from '../lib/types/app-request';
 import { TaskEntity } from '../lib/types/task.entity';
 
-import { TaskState } from '../lib/variables/task-state';
-import { IQueryParam } from '../lib/types/query-params';
-import { SortDirection } from '../lib/variables/sort-direction';
 import { authMiddleware } from '../lib/middleware/auth.middleware';
 import { taskBodyCheck } from '../validation/task-body-validation';
-import { taskTagParamIDCheck } from '../validation/taskTag-param-id-validation';
 import { handleReqQueryError } from '../lib/middleware/handle-err.middleware';
 import { taskQueryParamCheck } from '../validation/task-query-param-validation';
+import { taskTagParamIDCheck } from '../validation/taskTag-param-id-validation';
 
 export const router = express.Router();
 
-// TODO: Научиться передавать тип QueryParams в параметры запроса, не писать отдельно;
 // Получить все задачи
 router.get(
   '/tasks',
@@ -33,46 +28,21 @@ router.get(
       throw new Error('User not found');
     }
 
-    const query = knex<TaskEntity>('task').select('*');
+    const query = knex<TaskEntity>('task').select('*').where({ user_id: req.user.id }).returning('*');
 
-    const queryParam: IQueryParam = {
-      limit: Number(req.query.limit),
-      state: req.query.state as TaskState,
-      sortColumn: String(req.query.sort),
-      direction: null,
-    };
-
-    switch (req.query.sort) {
-      case 'created-at':
-        queryParam.sortColumn = 'created_at';
-        queryParam.direction = SortDirection.DESC;
-        break;
-      case 'name':
-        queryParam.sortColumn = 'name';
-        queryParam.direction = SortDirection.ASC;
-        break;
-      default:
-        queryParam.sortColumn = 'name';
-        queryParam.direction = SortDirection.ASC;
-        break;
+    if (req.query.limit) {
+      query.limit(Number(req.query.limit));
     }
-
-    if (queryParam.limit) {
-      query.limit(queryParam.limit);
-    }
-
     if (req.query.state) {
-      query.where({ state: queryParam.state });
+      query.where({ state: req.query.state });
+    }
+    if (req.query.sortDirection && req.query.sortProperty) {
+      query.orderBy(String(req.query.sortProperty), String(req.query.sortDirection));
     }
 
-    if (req.query.sort) {
-      query.orderBy(queryParam.sortColumn, queryParam.direction);
-    }
+    const finalResWithParam = await query;
 
-    const getAllTask = await query;
-
-    res.status(200).send(getAllTask);
-    return;
+    res.status(200).send(finalResWithParam);
   },
 );
 
@@ -118,7 +88,7 @@ router.post(
       throw new Error('User not found');
     }
 
-    const { name, description, state }: TaskEntity = req.body;
+    const { name, description, state }: Partial<TaskEntity> = req.body;
 
     const [task] = await knex<TaskEntity>('task')
       .insert({
@@ -150,7 +120,7 @@ router.put(
       throw new Error('User not found');
     }
 
-    const { name, description, state } = req.body;
+    const { name, description, state }: Partial<TaskEntity> = req.body;
 
     const query = await knex<TaskEntity>('task')
       .where({ user_id: req.user.id, id: req.params.id })
