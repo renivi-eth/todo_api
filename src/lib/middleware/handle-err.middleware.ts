799063bc1486d1d626901c333dfd96494e8538f8
ivin <ivinegor88@gmail.com>
2024-07-27 02:17:40 +0300

    fix(handle_err.middleware): fix Request type

--- src/lib/middleware/handle-err.middleware.ts
+++ src/lib/middleware/handle-err.middleware.ts
@@ -1,9 +1,10 @@
 import { validationResult } from 'express-validator';
 import { Request, Response, NextFunction } from 'express';
+import { AppRequest } from '../types/app-request';
 
 //Проверяет обнаружены ли ошибки при обработке параметров(query, body, params) от express-validator
 
-export const handleReqQueryError = (req: Request, res: Response, next: NextFunction) => {
+export const handleReqQueryError = (req: AppRequest, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
