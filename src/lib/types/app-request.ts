11d99bc0b43409bb9d06e21a7cc88c892dc4b1e9
ivin <ivinegor88@gmail.com>
2024-07-27 02:14:03 +0300

    fix(app-request): extend Request type

--- src/lib/types/app-request.ts
+++ src/lib/types/app-request.ts
@@ -1,6 +1,45 @@
+import { ParsedQs } from 'qs';
 import { Request } from 'express';
 import { IUserJWT } from './user-jwt';
+import { TaskState } from '../variables/task-state';
+import { SortDirection } from '../variables/sort-direction';
 
-export interface AppRequest extends Request {
-  user?: IUserJWT;
+// TODO: на review Рустаму
+/*
+Объект Request наполняет запрос от пользователя. 
+Имеет типизацию req: Request 
+
+Request выглядит как generic,который принимает след типы: 
+Request<Params = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = ParsedQs> - 
+выше мы типизируем свойства объекта, ключи любые, свойства - типы который указаны 
+Generic - это обобщенный тип, мы можем передать тип / интерфейс в этот generic и расширять объект запроса.
+
+Нам необходимо расширить наш Request что он понимал какие типы свойств будут у Query Param. 
+Query param по дефолту имеет тип ParsedQS 
+interface ParsedQs {
+        [key: string]: undefined | string | string[] | ParsedQs | ParsedQs[] | SortDirection;
+    }
+Который ключом принимает строку, типом undefined, string, массив String, ParsedQS, массив ParsedQS (ParsedQS, массив ParsedQS - значения для ключа может быть другим объектом, т.е. мы может принимать объект или массив каких либо query param )
+
+Поэтому мы делаем ExtendedParsedQs extends ParsedQs и расширяем типы, иными словами если придет query parameters limit - значение должно быть строкой и т.д,
+
+Почему расширение типа не ругается, ведь мы передаем два тип enum, а interface ParsedQS не может принимать enum. Ответ: enum читаются как строки или числа 
+
+*/
+
+// TODO:
+interface ExtendedParsedQs extends ParsedQs {
+  limit?: string;
+  state?: TaskState;
+  sortProperty?: string;
+  SortDirection?: SortDirection;
 }
+
+interface Params {
+  id: string;
+}
+
+// Переопределяем тип Request
+export type AppRequest = Request<Params, {}, {}, ExtendedParsedQs> & {
+  user?: IUserJWT;
+};
