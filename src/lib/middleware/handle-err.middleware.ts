0a061496fbdfc0a6e2a55571deb12e3ea4631130
ivin <ivinegor88@gmail.com>
2024-07-28 20:57:38 +0300

    fix(handle-err): fix syntax

--- src/lib/middleware/handle-err.middleware.ts
+++ src/lib/middleware/handle-err.middleware.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from 'express';
 import { AppRequest } from '../types/app-request';
 
 //Проверяет обнаружены ли ошибки при обработке параметров(query, body, params) от express-validator
-
 export const handleReqQueryError = (req: AppRequest, res: Response, next: NextFunction) => {
   const errors = validationResult(req);
 
