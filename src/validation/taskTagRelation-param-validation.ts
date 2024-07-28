75b92eaba6ccc149e5847c0ff9611491636a48cb
ivin <ivinegor88@gmail.com>
2024-07-28 20:19:21 +0300

    fix(validation, param-valid): delete trim()

--- src/validation/taskTagRelation-param-validation.ts
+++ src/validation/taskTagRelation-param-validation.ts
@@ -3,7 +3,4 @@ import { param } from 'express-validator';
 naming - entityWhat_validate(param, body,query)Check
 Проверка параметров taskId, tagID в testTag связи
 */
-export const taskTagIdCheck = [
-  param('taskId', 'ID must be UUID').trim().isUUID(),
-  param('tagId', 'ID must be UUID').trim().isUUID(),
-];
+export const taskTagIdCheck = [param('taskId', 'ID must be UUID').isUUID(), param('tagId', 'ID must be UUID').isUUID()];
