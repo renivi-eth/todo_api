b7f7531c2e2c0c9c7186e1d25f54a5f38fc97820
ivin <ivinegor88@gmail.com>
2024-07-29 19:54:30 +0300

    fix(app request): extend Params interface

--- src/lib/types/app-request.ts
+++ src/lib/types/app-request.ts
@@ -4,6 +4,9 @@ import { IUserJWT } from './user-jwt';
 import { TaskState } from '../variables/task-state';
 import { SortDirection } from '../variables/sort-direction';
 
+//TODO: подумай блять дважды!
+import * as core from 'express-serve-static-core';
+
 // TODO: на review Рустаму
 /*
 Объект Request наполняет запрос от пользователя. 
@@ -14,12 +17,12 @@ Request<Params = ParamsDictionary, ResBody = any, ReqBody = any, ReqQuery = Pars
 выше мы типизируем свойства объекта, ключи любые, свойства - типы который указаны 
 Generic - это обобщенный тип, мы можем передать тип / интерфейс в этот generic и расширять объект запроса.
 
-Нам необходимо расширить наш Request что он понимал какие типы свойств будут у Query Param. 
+Нам необходимо расширить наш Request чтоб он понимал какие типы свойств будут у Query Param. 
 Query param по дефолту имеет тип ParsedQS 
 interface ParsedQs {
         [key: string]: undefined | string | string[] | ParsedQs | ParsedQs[] | SortDirection;
     }
-Который ключом принимает строку, типом undefined, string, массив String, ParsedQS, массив ParsedQS (ParsedQS, массив ParsedQS - значения для ключа может быть другим объектом, т.е. мы может принимать объект или массив каких либо query param )
+Который ключом принимает строку, значением - undefined, string, массив String, ParsedQS, массив ParsedQS (ParsedQS, массив ParsedQS - значения для ключа может быть другим объектом, т.е. мы может принимать объект или массив каких либо query param )
 
 Поэтому мы делаем ExtendedParsedQs extends ParsedQs и расширяем типы, иными словами если придет query parameters limit - значение должно быть строкой и т.д,
 Почему расширение типа не ругается, ведь мы передаем два тип enum, а interface ParsedQS не может принимать enum. Ответ: enum читаются как строки или числа 
@@ -34,7 +37,7 @@ interface ExtendedParsedQs extends ParsedQs {
 }
 
 // Типизация для req.params.id
-interface Params {
+interface Params extends core.ParamsDictionary {
   id: string;
   taskId: string;
   tagId: string;
