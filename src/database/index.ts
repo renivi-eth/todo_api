2edf811fd4fc767196b7e46706b6b0c7052c2468
renivi <ivinegor88@gmail.com>
2025-04-24 01:07:23 +0300

    fix: comment

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,7 @@ import Knex from 'knex';
 
 dotenv.config();
 
-// Создание экземпляра подключения к PostgreSQL с помощью knex.js
+// Создание экземпляра подключения к PostgreSQL с помощью Knex.js
 export const knex = Knex({
   client: 'pg',
 
